bc716e3a3a2aca606a0878f2b12c0cfbd5a29222
Ryan.K <ryankung@ieee.org>
2017-07-19 23:40:53 +0800

    postgres disconnection

--- celebi/io/postgres/arbiter.py
+++ celebi/io/postgres/arbiter.py
@@ -1,5 +1,10 @@
 import asyncpg
 from pulsar.apps import Application
+from functools import partial
+
+
+async def test(self, exec=None):
+    print('test')
 
 
 class PostgresArbiter(Application):
@@ -9,14 +14,31 @@ class PostgresArbiter(Application):
         self.configs = configs
         super().__init__(**kv)
 
-    async def connect(self) -> asyncpg.Connection:
+    async def connect(self, *args, **kwargs) -> asyncpg.Connection:
         conn = await asyncpg.connect(**self.configs)
-        self.conn = conn
         return conn
 
+    async def disconnect(self, monitor, conn, *args, **kwargs) -> bool:
+        try:
+            await conn.close()
+            return True
+        except Exception as e:
+            raise(e)
+        finally:
+            return False
+
+    async def execute(self, worker):
+        pass
+
     async def monitor_start(self, monitor):
-        print('monitor starting')
-        self.con = await self.connect()
+        print('start')
+        conn = await self.connect()
+        monitor.conn = conn
+        monitor.event('stopping').bind(
+            partial(self.disconnect, conn=monitor.conn))
+
+    def monitor_stop(self, monitor):
+        self.disconnect()
 
     async def worker_start(self, worker, exc=None):
         print('worker start')
--- tests/io/posrgres/test_postgres.py
+++
@@ -1,6 +0,0 @@
-from tests.io.posrgres import TestPostgresProcess
-
-
-class TestPostgres(TestPostgresProcess):
-    def test_connection(self):
-        pass
