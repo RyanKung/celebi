237b7e79be8f6bfe72c37a1270835a46489794a8
Ryan.K <ryankung@ieee.org>
2017-08-21 02:22:55 +0800

    passed the unittest

--- celebi/apis/data.py
+++ celebi/apis/data.py
@@ -2,6 +2,7 @@ from pulsar.apps.wsgi import WsgiResponse, WsgiRequest
 from celebi.core.wsgi import wsgi
 from celebi.core.decorators import jsonrpc
 from celebi.types import Datum, Data
+import json
 
 __all__ = ['datum', 'Data']
 
@@ -9,7 +10,7 @@ __all__ = ['datum', 'Data']
 @wsgi.router('/datum', methods=['GET', 'POST'])
 @jsonrpc
 async def datum(request: WsgiRequest) -> WsgiResponse:
-    data = request.body_data()
+    data = json.loads(await request.body_data())
     return await {
         'POST': Datum.create
 
@@ -19,7 +20,7 @@ async def datum(request: WsgiRequest) -> WsgiResponse:
 @wsgi.router('/data', methods=['GET', 'POST', 'PUT', 'DELETE'])
 @jsonrpc
 async def data(request: WsgiRequest) -> WsgiResponse:
-    data = request.body_data()
+    data = json.loads(await request.body_data())
     return await {
         'POST': Datum.create
 
--- tests/apis/test_data.py
+++ tests/apis/test_data.py
@@ -1,12 +1,18 @@
 from tests.apis import TestCelebiThread
+import json
 
 
 class TestMeta(TestCelebiThread):
 
-    async def test_datum(self):
+    async def test_data(self):
         c = self.client
-        url = self.uri + '/datum'
-        response = await c.post(url, data={})
+        url = self.uri + '/data'
+        response = await c.post(url, data=json.dumps(dict(name='test',
+                                                          is_spout=False,
+                                                          generator='',
+                                                          comment='',
+                                                          flying=True,
+                                                          rate=1)))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content,
                          b'{"result": {}, "error": null, "id": 1}')
